a588f5e171a3a7b93b77a787156203afd3c14688
Luis Carlos Reyes Solarte <reyesluiscarlos920@gmail.com>
2026-05-05 17:09:34 -0500

    feat: :sparkles: Mejor visualización de usuarios.
    A la hora de realizar un get, los usuarios muestran solo los campos que no estén vacíos y que estén admitidos como campos (usuario, email, si es tutor/estudiante), además de que en el get no se vean los superusuarios si el get es realizado por un usuario que no sea staff.

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
 
 from .models import User
@@ -6,4 +8,15 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "is_tutor", "is_student"]
+        fields = ["id", "username", "email", "is_tutor", "is_student", "date_joined"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        return OrderedDict(
+            [
+                (key, value)
+                for key, value in ret.items()
+                if value not in [None, "", [], {}]
+            ]
+        )
--- users/views.py
+++ users/views.py
@@ -7,3 +7,10 @@ from .serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return User.objects.all()
+
+        return User.objects.filter(is_superuser=False)
