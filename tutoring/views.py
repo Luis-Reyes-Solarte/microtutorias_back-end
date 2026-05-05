722ffc38294b1a4c7e907d15ee6af9c4d8a2f059
Luis Carlos Reyes Solarte <reyesluiscarlos920@gmail.com>
2026-05-05 09:50:43 -0500

    feat: Configuraciones varias de DRF. Añadido de los requerimientos. Añadido de endpoints

--- tutoring/views.py
+++ tutoring/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import TutoringAd
+from .serializers import TutoringAdSerializer
+
 
 # Create your views here.
+class TutoringListView(generics.ListAPIView):
+    queryset = TutoringAd.objects.all()
+    serializer_class = TutoringAdSerializer
